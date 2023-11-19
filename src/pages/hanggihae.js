81696a115f7dd232301dc9f5441c2f80641bbd2b
rlawogms <dunk7119@gmail.com>
2023-11-19 20:41:42 +0900

    절댓값을 상댓값으로 변경 and 일시정지 버튼 + 페이징 추가 (기능은 없음)

---
+++ src/pages/hanggihae.js
@@ -0,0 +1,12 @@
+let state = 0;
+
+function change() {
+    if(state == 0){
+        state = 1;
+        document.getElementById('StartStop').src="../assets/images/start.png";
+    }
+    else {
+        state = 0;
+        document.getElementById('StartStop').src = "../assets/images/stop.png";
+    }
+}
